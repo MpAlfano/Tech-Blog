c925a8fa6bc336052c33e0d79f8bd500d3dbd62e
Mark Alfano <mpalfano4@gmail.com>
2022-02-28 23:27:49 -0500

    adding views

---
+++ public/js/posts.js
@@ -0,0 +1,51 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-title").value.trim();
+  const description = document.querySelector("#post-description").value.trim();
+
+  // update post
+
+  if (title && description) {
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create post");
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete post");
+    }
+  }
+};
+
+//selector for update
+//selector for each post to go into it and see comments
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".post-list")
+  .addEventListener("click", delButtonHandler);
