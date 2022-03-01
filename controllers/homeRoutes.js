4b5fb0f69305040c15b5bafde49ddb029923c551
Mark Alfano <mpalfano4@gmail.com>
2022-02-28 23:03:40 -0500

    updated routes recycled code from lessions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,84 @@
 const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
-const { Blog } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all Posts and JOIN with user data
+    const PostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          model: Comment,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const Posts = PostData.map((Post) => Post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      Posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/Post/:id", async (req, res) => {
+  try {
+    const PostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          model: Comment,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const Post = PostData.get({ plain: true });
+
+    res.render("Post", {
+      ...Post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,7 +3,36 @@ const Comment = require("./Comment");
 const User = require("./User");
 
 //user has many posts, user has many comments
-//posts belong to user posts
-//posts has many
+//posts belong to user
+//posts has many comments
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignkey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignkey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = { Post, Comment, User };
